b4850bcfca77d170bc0434d03cbec8afe99c3efe
Nazmul Hoque <nazmul.hoque@gmail.com>
2022-12-11 10:44:14 -0500

    Update PersonList.js

--- src/PersonList.js
+++ src/PersonList.js
@@ -1,1 +1,29 @@
+import React from 'react';
+import axios from 'axios';
 
+export default  PersonList  {
+  state = {
+    persons: []
+  }
+
+ 
+  axios.get(`https://jsonplaceholder.typicode.com/users`)
+    .then(res => {
+      const persons = res.data;
+      this.setState({ persons });
+    })
+
+
+
+  return (
+    <ul>
+      {
+        this.state.persons
+          .map(person =>
+            <li key={person.id}>{person.name}</li>
+          )
+      }
+    </ul>
+  )
+
+}
