66ea6dda63cdbddfbb683691c91cf440008ba181
Nazmul Hoque <nazmulh@procom.ca>
2022-12-11 13:36:34 -0500

    minor change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PersonAdd from './PersonAdd.js';
 import PersonList from './PersonList.js'
-import PersonRemove from './PersonRemove.js';
+
 
 function App() {
   
   return <div>
     <PersonList />
-    {/* <PersonRemove/> */}
     <PersonAdd/>
     </div>;
 }
